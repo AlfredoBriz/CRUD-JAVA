8392bfaf7781f8bfd0f741e196bd4110ddb7780a
alfredo brizuela <alfredobrizuela1@gmail.com>
2022-02-08 17:19:40 -0300

    Release 1: CRUD

---
+++ CRUDArrayList.java
@@ -0,0 +1,16 @@
+package crudarraylist;
+
+/**
+ *
+ * @author Alfredo Brizuela
+ */
+public class CRUDArrayList {
+
+   
+    public static void main(String[] args) {
+        Vista vista = new Vista();
+        vista.setVisible(true);
+
+    }
+    
+}
